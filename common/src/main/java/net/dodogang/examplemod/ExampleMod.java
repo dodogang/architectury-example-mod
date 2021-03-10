643267e2ac8b8d3b5fa6ca60b3b8669f4e790820
Trikzon <trikzondev@gmail.com>
2021-03-09 19:37:04 -0500

    Add Plume Dependency and Examples

--- common/src/main/java/net/dodogang/examplemod/ExampleMod.java
+++ common/src/main/java/net/dodogang/examplemod/ExampleMod.java
@@ -1,35 +1,21 @@
 package net.dodogang.examplemod;
 
-//import me.shedaniel.architectury.registry.CreativeTabs;
-//import me.shedaniel.architectury.registry.DeferredRegister;
-//import me.shedaniel.architectury.registry.Registries;
-//import me.shedaniel.architectury.registry.RegistrySupplier;
-//import net.minecraft.core.Registry;
-//import net.minecraft.resources.ResourceLocation;
-//import net.minecraft.util.LazyLoadedValue;
-//import net.minecraft.world.item.CreativeModeTab;
-//import net.minecraft.world.item.Item;
-//import net.minecraft.world.item.ItemStack;
+import net.dodogang.plume.ash.registry.ItemGroupBuilder;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ExampleMod {
     public static final String MOD_ID = "examplemod";
-    // We can use this if we don't want to use DeferredRegister
-//    public static final LazyLoadedValue<Registries> REGISTRIES = new LazyLoadedValue<>(() -> Registries.get(MOD_ID));
-    // Registering a new creative tab
-//    public static final CreativeModeTab EXAMPLE_TAB = CreativeTabs.create(new ResourceLocation(MOD_ID, "example_tab"), new Supplier<ItemStack>() {
-//        @Override
-//        public ItemStack get() {
-//            return new ItemStack(EXAMPLE_ITEM.get());
-//        }
-//    });
-    
-//    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(MOD_ID, Registry.ITEM_REGISTRY);
-//    public static final RegistrySupplier<Item> EXAMPLE_ITEM = ITEMS.register("example_item", () ->
-//            new Item(new Item.Properties().tab(ExampleMod.EXAMPLE_TAB)));
-    
-    public static void init() {
-//        ITEMS.register();
-        
-        System.out.println(ExampleExpectPlatform.getConfigDirectory().toAbsolutePath().normalize().toString());
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+
+    public static final ItemGroup ITEM_GROUP = ItemGroupBuilder
+            .build(new Identifier(MOD_ID, "item_group"), () -> new ItemStack(Blocks.DIRT));
+
+    public static void initialize() {
+        ModBlocks.register();
     }
 }
